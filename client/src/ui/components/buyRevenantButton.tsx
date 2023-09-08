0e526fad5a5e52eddfd0a4a202167aba6cb9eedf
Alexhalo <alexblockchain.3115@gmail.com>
2023-09-08 21:08:54 +0100

    Small typo fix

--- client/src/ui/components/buyRevenantButton.tsx
+++ client/src/ui/components/buyRevenantButton.tsx
@@ -68,14 +68,7 @@ export const BuyRevenantButton = ({ layer }: ExampleComponentProps) => {
         className="cool-button"
         onClick={() => create_outpost(account, GAME_ID, getComponentValue(GameData, entityId)?.count_outposts || 0)}
       >
-        buy revenant, 1 amount of outposts:{getComponentValue(GameData, entityId)?.count_outposts || 0}
-      </button>
-
-      <button
-        className="cool-button"
-        onClick={() => create_outpost(account, GAME_ID, getComponentValue(GameData, entityId)?.count_outposts || 0)}
-      >
-        buy revenant 2
+        buy revenant, amount of outposts:{getComponentValue(GameData, entityId)?.count_outposts || 0}
       </button>
 
       <button
